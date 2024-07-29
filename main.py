23ee096efa8ba120d0bedead497f71ae4a1c9e3b
Roberto Fernández <61292228+robertofdzb10@users.noreply.github.com>
2024-07-29 11:49:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 if __name__ == '__main__':
     import tkinter as tk
     from TicTacToe.TicTacToeApp import TicTacToeApp
+    from robot.BoardSizeSelectionApp import BoardSizeSelectionApp
     import warnings
     
     warnings.filterwarnings("ignore", category=UserWarning)
 
     root = tk.Tk()
-    app = TicTacToeApp(root)
+    selection_app = BoardSizeSelectionApp(root)
     root.mainloop()
